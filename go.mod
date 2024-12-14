aa908c58d86d664e9e4c126922fb5f01fdaa7882
tathagat <tathagat.reimann@gmail.com>
2024-12-14 15:14:07 +0100

    dirty REST server to call piper and return voice file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tathagat-reimann/piper-api
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,75 @@
+// create a web server
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type TextToConvert struct {
+	Text string
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, World!\n")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/converTextToVoice", converTextToVoice)
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func converTextToVoice(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.Body)
+	decoder := json.NewDecoder(r.Body)
+	var t TextToConvert
+	err := decoder.Decode(&t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Println("Will try to convert " + t.Text + " to voice")
+
+	piper_command := "echo '" + t.Text + "' | /home/tathagat/tmp/piper/piper --model /home/tathagat/tmp/piper/voices/en_US-hfc_male-medium.onnx -d /home/tathagat/tmp/piper/output/"
+	out, err := exec.Command("bash", "-c", piper_command).Output()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	generated_file := strings.TrimSpace(string(out))
+
+	log.Printf("Piper generated voice file: %s", generated_file)
+
+	/*
+		stat, _ := os.Stat(generated_file)
+		log.Println(stat)
+	*/
+
+	//http.ServeFile(w, r, string(out))
+	w.Header().Set("Content-Disposition", "attachment;")
+	file, err := os.ReadFile(generated_file)
+	if err != nil {
+		//panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(file)
+}
